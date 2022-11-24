7f8d7326861eaf183774badc045df5d674541d63
Pati Rocha <patirocha.dev@gmail.com>
2022-11-24 16:44:49 -0300

    update footer.jsx, header.jsx, header.module.css and menu.module.css

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,9 +1,9 @@
 import styles from "./footer.module.css"
 
-export function Footer ({name}) {
+export function Footer () {
     return (
         <footer className={styles.footer}>
-            <p  className={styles.paragraph}> &copy;{name} ♡ - Todos os direitos são reservados. </p>
+            <p  className={styles.paragraph}> &copy;Pati Rocha ♡ - Todos os direitos são reservados. </p>
         </footer>
     )
 }
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,7 +4,7 @@ export function Header({ title, image }) {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>{title}</h1>
-      <img className={styles.image} src={image} alt="ilustração mulher sentada de perfil com cabelo preso usando um terno preto apoiando um notbook cinza no colo" />
+      <img className={styles.image} src={image} alt="" />
     </header>
   );
 }
