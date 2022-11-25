518ec5c3922c0e4bc95dc824b7ea78ece79ffe60
Pati Rocha <patirocha.dev@gmail.com>
2022-11-25 13:55:50 -0300

    update repositories.module.css and projects.jsx

--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -1,14 +1,16 @@
 import { Header } from "../../components/Header/header";
 import portImg from "../../assets/portfolio.svg"
-import styles from "./projects.module.css"
+import { Repositories } from "../../components/Repositories/repositories";
+// import styles from "./projects.module.css"
 
 export function Projects(){
     return(
         <>
-        <Header title="Meus Repositórios" image={portImg} alt="imagem" />   
-        <main className={styles.main}>
+        <Header title="Meus Repositórios" image={portImg} alt="imagem" />  
+        <Repositories />
+        {/* <main className={styles.main}>
             <p> Em desenvolvimento...</p>
-        </main>
+        </main> */}
                
         </>
     ) 
