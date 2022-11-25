87f69377bb58ef213b63cb13f5f4db830480636d
Pati Rocha <patirocha.dev@gmail.com>
2022-11-25 13:37:32 -0300

    update Portfolio.jsx

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,12 +1,15 @@
 import { Header } from "../../components/Header/header";
 import portImg from "../../assets/portfolio.svg"
-import { Repositories } from "../../components/Repositories/repositories";
+import styles from "./portfolio.module.css"
 
 export function Portfolio(){
     return(
         <>
         <Header title="Meus Repositórios" image={portImg} alt="imagem" />   
-        <Repositories />         
+        <main className={styles.main}>
+            <p> Em desenvolvimento...</p>
+        </main>
+               
         </>
     ) 
 }
